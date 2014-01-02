7701ef4c28dfb2058c287ce2d9381b74b19d8d96
Yuri Pimenov <pimenoff@pokeroff.ru>
2014-01-02 12:26:50 +0200

    Debug

--- lib/themes_for_rails/assets_controller.rb
+++ lib/themes_for_rails/assets_controller.rb
@@ -28,6 +28,11 @@ module ThemesForRails
     end
     
     def find_themed_asset(asset_name, asset_theme, asset_type, &block)
+      puts asset_name
+      puts asset_theme
+      puts asset_type
+      raise 'x'
+
       if asset_type == 'stylesheets' 
         path = asset_stylesheets_path(asset_name, asset_theme, asset_type)
       else
